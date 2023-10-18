b71e2c263c8163d96b25d9e63513c602c7e8c68f
Charlie1991 <juancarlosnbcc@gmail.com>
2023-10-18 14:04:32 -0600

    Library added

--- Second-Option/script.js
+++ Second-Option/script.js
@@ -1,5 +1,6 @@
 let menubar = document.querySelector("#menu-bar");
 let mynav = document.querySelector(".nav");
+let searchbar = document.querySelector("#search-bar");
 
 manubar.onclick = () => {
   menybar.classList.toggle("fa.times");

a5936edd0c46211bd14b311b52726e9ec30bfd75
Charlie1991 <juancarlosnbcc@gmail.com>
2023-10-18 21:03:11 -0600

    Added images

--- Second-Option/script.js
+++ Second-Option/script.js
@@ -1,8 +1,13 @@
 let menubar = document.querySelector("#menu-bar");
 let mynav = document.querySelector(".nav");
 let searchbar = document.querySelector("#search-bar");
+let search = document.querySelector(".search-form");
 
 manubar.onclick = () => {
   menybar.classList.toggle("fa.times");
   mynav.classList.toggle("active");
+};
+
+searchbar.onclick = () => {
+  search.classList.toggle("active");
 };
